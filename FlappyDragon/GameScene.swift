342c113ba364f077a974812cb6594aaff2758715
Higor <higorjugler@hotmail.com>
2024-03-20 09:55:11 -0300

    Feature para add inimigos

--- FlappyDragon/GameScene.swift
+++ FlappyDragon/GameScene.swift
@@ -15,6 +15,12 @@ class GameScene: SKScene {
     var player: SKSpriteNode!
     var gameArea: CGFloat = 410.0
     var velocity: Double = 100.0
+    var gameFinished = false
+    var gameStarted = false
+    var restart = false
+    var scoreLabel: SKLabelNode!
+    var score: Int = 0
+    var flyForce: CGFloat = 30.0
     
     override func didMove(to view: SKView) {
         addBackground()
@@ -36,6 +42,22 @@ class GameScene: SKScene {
         floor.zPosition = 2
         floor.position = CGPoint(x: floor.size.width/2, y: size.height - gameArea - floor.size.height/2)
         addChild(floor)
+        
+        // Criando fisica para chao e teto
+        let invisibleFloor = SKNode()
+        invisibleFloor.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: size.width, height: 1))
+        invisibleFloor.physicsBody?.isDynamic = false
+        invisibleFloor.position = CGPoint(x: size.width/2, y: size.height - gameArea)
+        invisibleFloor.zPosition = 2
+        addChild(invisibleFloor)
+        
+        let invisibleRoof = SKNode()
+        invisibleRoof.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: size.width, height: 1))
+        invisibleRoof.physicsBody?.isDynamic = false
+        invisibleRoof.position = CGPoint(x: size.width/2, y: size.height)
+        invisibleRoof.zPosition = 2
+        addChild(invisibleRoof)
+        
     }
     
     func addIntro() {
@@ -49,7 +71,7 @@ class GameScene: SKScene {
         player = SKSpriteNode(imageNamed: "player1")
         player.zPosition = 4
         player.position = CGPoint(x: 60, y: size.height - gameArea/2)
-
+        
         var playerTexture = [SKTexture]()
         for i in 1...4 {
             playerTexture.append(SKTexture(imageNamed: "player\(i)"))
@@ -71,12 +93,81 @@ class GameScene: SKScene {
         floor.run(repeatAction)
         
     }
- 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+    func addScore() {
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.fontSize = 94
+        scoreLabel.text = "\(score)"
+        scoreLabel.zPosition = 5
+        scoreLabel.position = CGPoint(x: size.width/2, y: size.height - 100)
+        scoreLabel.alpha = 0.8
+        addChild(scoreLabel)
+    }
+    
+    func spawnEnemies() {
+        let initialPosition = CGFloat(arc4random_uniform(132) + 74)
+        let enemyNumber = Int(arc4random_uniform(4) + 1)
+        let enemiesDistance = self.player.size.height * 2.5
+        
+        let enemyTop = SKSpriteNode(imageNamed: "enemytop\(enemyNumber)")
+        
+        let enemyWidth = enemyTop.size.width
+        let enemyHeight = enemyTop.size.height
+        
+        enemyTop.position = CGPoint(x: size.width + enemyWidth/2, y: size.height - initialPosition + enemyHeight/2)
+        enemyTop.zPosition = 1
+        enemyTop.physicsBody = SKPhysicsBody(rectangleOf: enemyTop.size)
+        enemyTop.physicsBody?.isDynamic = false
+        
+        let enemyBottom = SKSpriteNode(imageNamed: "enemybottom\(enemyNumber)")
+        
+        enemyBottom.position = CGPoint(x: size.width + enemyWidth/2, y: enemyTop.position.y - enemyTop.size.height - enemiesDistance)
+        enemyBottom.zPosition = 1
+        enemyBottom.physicsBody = SKPhysicsBody(rectangleOf: enemyTop.size)
+        enemyBottom.physicsBody?.isDynamic = false
+        
+        let distance = size.width + enemyWidth
+        let duration = Double(distance)/velocity
+        let moveAction = SKAction.moveBy(x: -distance, y: 0, duration: duration)
+        let removeAction = SKAction.removeFromParent()
+        let sequenceAction = SKAction.sequence([moveAction, removeAction])
+        
+        enemyTop.run(sequenceAction)
+        enemyBottom.run(sequenceAction)
+        
+        addChild(enemyTop)
+        addChild(enemyBottom)
         
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !gameFinished {
+            if !gameStarted {
+                intro.removeFromParent()
+                addScore()
+                
+                // Construindo elemento de fisica para sprite
+                player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width/2 - 10)
+                player.physicsBody?.isDynamic = true
+                player.physicsBody?.allowsRotation = true
+                player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: flyForce))
+                
+                gameStarted = true
+                
+                // Inserindo inimigos na cena
+                Timer.scheduledTimer(withTimeInterval: 2.5, repeats: true, block: { (timer) in self.spawnEnemies() })
+                
+            } else {
+                player.physicsBody?.velocity = CGVector.zero
+                player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: flyForce))
+            }
+        }
+    }
+    
     override func update(_ currentTime: TimeInterval) {
-        
+        if gameStarted {
+            let yVelocity = player.physicsBody!.velocity.dy * 0.001 as CGFloat
+            player.zRotation = yVelocity
+        }
     }
 }
